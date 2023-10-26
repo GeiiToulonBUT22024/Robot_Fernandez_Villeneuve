7b66637050fd85627088b4349b343dca7cde7180
TABLE2 <rct@univ-tln.fr>
2023-10-26 11:08:15 +0200

    InnterfacerobotS1

---
+++ C#/InterfaceRobot/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExtendedSerialPort;
+
+
+namespace InterfaceRobot
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ReliableSerialPort serialPort1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialPort1 = new ReliableSerialPort("COM6", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.Open();
+        }
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+
+            //if (buttonEnvoyer.Background == Brushes.RoyalBlue)
+            //{
+            //    buttonEnvoyer.Background = Brushes.Beige; //Quand on clique ça change la couleur du bouton, pour la 1, si on appui pls fois ça change le premier coup puis pas après
+            //}
+            //else
+            //{
+            //    buttonEnvoyer.Background = Brushes.RoyalBlue;
+            //}
+
+            textBoxReception.Text += "Reçu: " + textBoxEmission.Text + "\n";
+            textBoxEmission.Text = "" ;
+
+        }
+
+            private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+
+        void SendMessage()
+        {
+            textBoxReception.Text += "Reçu: " + textBoxEmission.Text + "\n";
+            //textBoxEmission.Text = "";
+
+            serialPort1.WriteLine(textBoxEmission.Text);
+        }
+    }
+}
